adaa9f0dc0ab97797abd87877ea85b48b5297c5e
Chris Strom <eee.c@eeecooks.com>
2010-07-04 22:30:05 -0400

    Rework the vows with this.callback

--- test/just_playing.js
+++ test/just_playing.js
@@ -1,5 +1,6 @@
 var vows = require('vows'),
-    assert = require('assert');
+  assert = require('assert'),
+    puts = require('sys').puts;
 
 require.paths.push("lib");
 var player_from_querystring = require('player_from_querystring').app;
@@ -8,16 +9,14 @@ var suite = vows.describe('just_playing').
   addBatch({
     'with a query string': {
       topic: function() {
-        var response;
-        function downstream (obj) { response = obj; }
-
-        var upstream_listener = player_from_querystring.call(downstream);
+        var topic = this;
+        var upstream_listener = player_from_querystring.call(
+          function(obj) { topic.callback(null, obj.body); }
+        );
         upstream_listener({
           url: { search : "?player=foo&x=1&y=2" },
           headers: { cookie: null }
         });
-
-        return response.body;
       },
 
       'is player': function (player) {
@@ -40,17 +39,15 @@ var suite = vows.describe('just_playing').
    addBatch({
      'POSTing data': {
        topic: function() {
-         var response;
-         function downstream (obj) { response = obj; }
-
-         var upstream_listener = player_from_querystring.call(downstream);
+         var topic = this;
+         var upstream_listener = player_from_querystring.call(
+           function(obj) { topic.callback(null, obj); }
+         );
          upstream_listener({body: "foo"});
-
-         return [response];
        },
 
-       'is null response': function (topic) {
-         assert.isUndefined (topic[0]);
+       'is null response': function (obj) {
+         assert.isUndefined (obj);
        }
      }
   }).export(module);
