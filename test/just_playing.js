a7fb9b9c2157c931bfa623a6296db930f031730f
Chris Strom <eee.c@eeecooks.com>
2010-07-06 05:21:48 -0400

    Refactor vows.js test with a macro

--- test/just_playing.js
+++ test/just_playing.js
@@ -5,19 +5,27 @@ var vows = require('vows'),
 require.paths.push("lib");
 var player_from_querystring = require('player_from_querystring').app;
 
+var api = {
+  fab: {
+    send_obj: function(obj) {
+      return function () {
+        var topic = this;
+        var upstream_listener = player_from_querystring.call(
+          function(obj) { topic.callback(null, obj && obj.body); }
+        );
+        upstream_listener(obj);
+      };
+    }
+  }
+};
+
 var suite = vows.describe('just_playing').
   addBatch({
     'with a query string': {
-      topic: function() {
-        var topic = this;
-        var upstream_listener = player_from_querystring.call(
-          function(obj) { topic.callback(null, obj.body); }
-        );
-        upstream_listener({
-          url: { search : "?player=foo&x=1&y=2" },
-          headers: { cookie: null }
-        });
-      },
+      topic: api.fab.send_obj({
+        url: { search : "?player=foo&x=1&y=2" },
+        headers: { cookie: null }
+      }),
 
       'is player': function (player) {
         assert.equal (player.id, "foo");
@@ -34,17 +42,9 @@ var suite = vows.describe('just_playing').
       'has Y coordinate': function(player) {
         assert.equal(player.y, 2);
       }
-    }
-  }).
-  addBatch({
+    },
     'POSTing data': {
-      topic: function() {
-        var topic = this;
-        var upstream_listener = player_from_querystring.call(
-          function(obj) { topic.callback(null, obj); }
-        );
-        upstream_listener({body: "foo"});
-      },
+      topic: api.fab.send_obj({body: "foo"}),
 
       'is null response': function (obj) {
         assert.isUndefined (obj);
