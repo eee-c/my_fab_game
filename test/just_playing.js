f2e6416b3d45ed6dfc90f9963ea5031d625d2a45
Chris Strom <eee.c@eeecooks.com>
2010-07-05 21:25:50 -0400

    Whitespace

--- test/just_playing.js
+++ test/just_playing.js
@@ -35,20 +35,19 @@ var suite = vows.describe('just_playing').
         assert.equal(player.y, 2);
       }
     }
-   }).
-   addBatch({
-     'POSTing data': {
-       topic: function() {
-         var topic = this;
-         var upstream_listener = player_from_querystring.call(
-           function(obj) { topic.callback(null, obj); }
-         );
-         upstream_listener({body: "foo"});
-       },
+  }).
+  addBatch({
+    'POSTing data': {
+      topic: function() {
+        var topic = this;
+        var upstream_listener = player_from_querystring.call(
+          function(obj) { topic.callback(null, obj); }
+        );
+        upstream_listener({body: "foo"});
+      },
 
-       'is null response': function (obj) {
-         assert.isUndefined (obj);
-       }
-     }
+      'is null response': function (obj) {
+        assert.isUndefined (obj);
+      }
+    }
   }).export(module);
-
