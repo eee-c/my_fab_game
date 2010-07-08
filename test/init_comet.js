246543fb3335a3b0ccb913122e82968a8c23a266
Chris Strom <eee.c@eeecooks.com>
2010-07-08 08:01:26 -0400

    Vows for init_comet. Add connection termination in absence of player

--- test/init_comet.js
+++ test/init_comet.js
@@ -7,17 +7,21 @@ var init_comet = require('init_comet').app;
 
 var api = {
   fab: {
-    when_upstream_replies_with: function(obj) {
+    when_upstream_replies_with: function(upstream_obj) {
       return function () {
         var topic = this;
 
-        var upstream = function() { this(obj); },
+        var upstream = function() { this(upstream_obj); },
             unary    = init_comet(upstream);
 
+        var chunks = [];
         unary.call(
-          function(obj) {
-            topic.callback(null, obj);
-            return function listener() {return listener;};
+          function listener(obj) {
+            chunks.push(obj);
+            if (obj == upstream_obj || typeof(obj) == "undefined") {
+              topic.callback(null, chunks);
+            }
+            return listener;
           }
         );
       };
@@ -29,8 +33,44 @@ var suite = vows.describe('init_comet').
   addBatch({
     'with a player': {
       topic: api.fab.when_upstream_replies_with({body: {id:1, uniq_id:42}}),
-      'sets a session cookie': function(obj) {
-        assert.equal(obj.headers["Set-Cookie"], "MYFABID=42");
+
+      'sets a session cookie': function(chunks) {
+        assert.equal(chunks[0].headers["Set-Cookie"], "MYFABID=42");
+      },
+
+      'sends the opening HTML doc': function(chunks) {
+        assert.match(chunks[1].body, /<html/i);
+      },
+
+      'send 1000+ bytes to get Chrome\'s attention': function(chunks) {
+        var byte_count = 0;
+        for (var i=0; i < chunks.length; i++) {
+          var chunk = chunks[i];
+          if (chunk && chunk.body && typeof(chunk.body) == "string") {
+            byte_count = byte_count + chunk.body.length;
+          }
+        }
+        assert.isTrue(byte_count > 1000);
+      },
+
+      'sends the player': function(chunks) {
+        assert.deepEqual(chunks[chunks.length-1], {body: {id:1, uniq_id:42}});
+      }
+    },
+
+    'without a player': {
+      topic: api.fab.when_upstream_replies_with(),
+
+      'terminates the downstream connection': function(chunks) {
+        assert.isUndefined(chunks[0]);
+      }
+    },
+
+    'with an invalid player object': {
+      topic: api.fab.when_upstream_replies_with({body:{}}),
+
+      'terminates the downstream connection': function(chunks) {
+        assert.isUndefined(chunks[0]);
       }
     }
   }).export(module);
