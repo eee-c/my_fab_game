bb9bf10eb25b8efda348ea035a8afa0e91712484
Chris Strom <eee.c@eeecooks.com>
2010-07-11 10:34:17 -0400

    Better names for the test helpers.

--- test/if_body_test.js
+++ test/if_body_test.js
@@ -5,73 +5,56 @@ var vows = require('vows'),
 require.paths.push("lib");
 var if_body = require('if_body').app;
 
-var api = {
-  fab: {
-    callback_when_send_obj: function(obj) {
-      return function () {
-        var topic = this;
-        var upstream_listener = if_body(
-          function(obj) { topic.callback(null, obj); }
-        ).call(function() {});
+function args_to_callback(obj) {
+  return function () {
+    var topic = this;
+    var upstream_listener = if_body(
+      function(obj) { topic.callback(null, obj); }
+    ).call(function() {});
 
-        upstream_listener(obj);
-      };
-    },
-    downstream_when_exception: function() {
-      return function () {
-        var topic = this;
-        var upstream_listener = if_body(
-          function() { throw "Foo!"; }
-        ).call(
-          function(obj) { topic.callback(null, obj); }
-        );
+    upstream_listener(obj);
+  };
+};
 
-        upstream_listener({body:"foo"});
-      };
-    },
-    downstream_when_send_obj: function(obj) {
-      return function () {
-        var topic = this;
-        var upstream_listener = if_body(
-          function() {}
-        ).call(
-          function(obj) { topic.callback(null, obj); }
-        );
+function downstream_response(obj, fn) {
+  if (!fn) fn = function() {};
+  return function () {
+    var topic = this;
+    var upstream_listener = if_body(fn).
+      call(function(obj) { topic.callback(null, obj); });
 
-        upstream_listener(obj);
-      };
-    }
-  }
+    upstream_listener(obj);
+  };
 };
 
 var suite = vows.describe('if_body').
   addBatch({
     'callback, with a body': {
-      topic: api.fab.callback_when_send_obj({body: "foo"}),
+      topic: args_to_callback({body: "foo"}),
       'invokes callback with body': function(obj) {
         assert.equal(obj, "foo");
       }
     },
     'downstream, with a body': {
-      topic: api.fab.downstream_when_send_obj({body: "foo"}),
+      topic: downstream_response({body: "foo"}),
       'terminates connection': function(obj) {
         assert.isUndefined(obj);
       }
     },
     'downstream, with HTTP headers': {
-      topic: api.fab.downstream_when_send_obj({headers: "foo"}),
+      topic: downstream_response({headers: "foo"}),
       'terminates connection': function(obj) {
         assert.isUndefined(obj);
       }
     },
     'downstream, with empty request': {
-      topic: api.fab.downstream_when_send_obj(),
+      topic: downstream_response(),
       'terminates connection': function(obj) {
         assert.isUndefined(obj);
       }
     },
     'downstream, with exception': {
-      topic: api.fab.downstream_when_exception(),
+      topic: downstream_response({body: "foo"}, function(){throw "foo";}),
       'terminates connection': function(obj) {
         assert.isUndefined(obj);
       }
