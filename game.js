6449203925fcbcbe93ad565e4d3ee125e914efc9
Chris Strom <eee.c@eeecooks.com>
2010-09-04 15:22:22 -0400

    Use process.on to handle uncaught exceptions. No crashy!

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+// Don't crash on errors
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err.stack);
+});
+
 var express = require('express'),
     http    = require('http'),
     faye    = require('faye'),
