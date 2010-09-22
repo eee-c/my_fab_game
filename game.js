d6e2dbbfa3ba4e0923d1022c2c7953ecbf213fdc
Chris Strom <eee.c@eeecooks.com>
2010-09-21 23:00:12 -0400

    Factor logger and faye_server_auth into modules

--- game.js
+++ game.js
@@ -7,7 +7,6 @@ process.on('uncaughtException', function (err) {
 
 var http = require('http'),
     faye = require('faye'),
-    puts = require( "sys" ).puts,
     inspect = require( "sys" ).inspect,
     couchdb = require('node-couchdb/lib/couchdb'),
     client  = couchdb.createClient(5984, 'localhost'),
@@ -17,62 +16,9 @@ var http = require('http'),
 fab.static = require('fab.static');
 fab.accept = require('fab.accept');
 
-Logger = {
-  level: 1,
-
-  debug:  function(msg) { if (this.level<1) puts("[DEBUG] " + msg); },
-  info:   function(msg) { if (this.level<2)  puts("[INFO] " + msg); },
-  warn:   function(msg) { if (this.level<3)  puts("[WARN] " + msg); },
-  errror: function(msg) { if (this.level<4)  puts("[ERROR] " + msg); }
-};
-
+var Logger = require("./lib/logger");
 
 Logger.info("Starting up...");
-
-// Server-side extension to lock player messages to client that added
-// the player in the first place,
-// http://japhr.blogspot.com/2010/08/per-message-authorization-in-faye.html
-var serverAuth = {
-  incoming: function(message, callback) {
-    // Let non-meta messages through
-    if (message.channel.indexOf('/meta/') === 0)
-      return callback(message);
-
-    Logger.debug("[Faye.incoming] " + message.channel);
-
-    // Get subscribed channel and auth token
-    var subscription = message.subscription,
-        msgToken     = message.ext && message.ext.authToken;
-
-    // Message does not have a player ID
-    if (!message.data.id)
-      return callback(message);
-
-    // Message has a player ID
-    Logger.debug("[Faye.incoming]  checking for player: " + message.data.id);
-
-    players.get(message.data.id, function(player) {
-      Logger.debug("[Faye.incoming]  " + inspect(player));
-
-      // If the player is already in the room
-      if (player) {
-        Logger.debug("[Faye.incoming]  token check: " + player.token + " " + msgToken);
-
-        // If the tokens do not match, stop the message
-        if (player.token != msgToken) {
-          Logger.warn("rejecting mis-matched token message");
-          message.error = 'Invalid player auth token';
-        }
-      }
-      else {
-        Logger.debug("[Faye.incoming]  " + message.data.id + " adding message token: " + msgToken);
-        message.data.authToken = msgToken;
-      }
-
-      callback(message);
-    });
-  }
-};
 
 function attach_faye(server) {
   var faye_server = new faye.NodeAdapter({
@@ -84,7 +30,7 @@ function attach_faye(server) {
 
   // attach the extension ensuring player messages come from the same
   // client that originally added player to the room
-  faye_server.addExtension(serverAuth);
+  faye_server.addExtension(require("./lib/faye_server_auth"));
 }
 
 var board_js_string =  " var player_list; \
