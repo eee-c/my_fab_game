debd68b0cbbf46367e6d16520109532f25884215
Chris Strom <eee.c@eeecooks.com>
2010-09-23 18:14:59 -0400

    Move the player store into its own module

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 
 // Don't crash on errors
 process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ' + err.stack);
+  console.log('Caught uncaughtException: ' + err.stack);
 });
 
 var http = require('http'),
@@ -11,27 +11,22 @@ var http = require('http'),
     couchdb = require('node-couchdb/lib/couchdb'),
     client  = couchdb.createClient(5984, 'localhost'),
     db      = client.db('my-fab-game'),
-    fab        = require('fab');
+    fab     = require('fab');
 
+// Additional (fab) apps
 fab.static = require('fab.static');
 fab.accept = require('fab.accept');
+
+
+// Player local store
+var players = (require("./lib/players")).init(db);
 
 var Logger = require("./lib/logger");
 
 Logger.info("Starting up...");
 
-function attach_faye(server) {
-  var faye_server = new faye.NodeAdapter({
-    mount:    '/faye',
-    timeout:  45
-  });
 
-  faye_server.attach(server);
-
-  // attach the extension ensuring player messages come from the same
-  // client that originally added player to the room
-  faye_server.addExtension(require("./lib/faye_server_auth"));
-}
+// Partials and in-page javascript
 
 var board_js_string =  " var player_list; \
 \
@@ -154,136 +149,7 @@ with ( html )
 ();
 
 
-// Player local store
-var players = ({
-  all: function(callback) {
-    return db.allDocs({include_docs:true}, function(err, docs) {
-      callback(docs.rows.map(function(row) {return row.doc;}));
-    });
-  },
-
-  get: function(id, callback) {
-    Logger.debug("[players.get] trying to get: " + id);
-    db.getDoc(id, function(err, res) {
-      if (err) {
-        Logger.warn(JSON.stringify(err));
-      }
-      callback(res);
-    });
-  },
-
-  add_player: function(player) {
-    var self = this;
-    var new_id = player.id;
-    Logger.info("players.add_player: " + new_id);
-    this.get(new_id, function(old_player) {
-      if (!old_player) {
-        db.saveDoc(new_id, {token: player.authToken});
-
-        delete(player['authToken']);
-        self.update_player_status(player);
-      }
-    });
-  },
-
-  update_player_status: function(status) {
-    var self = this;
-    this.get(status.id, function(player) {
-      Logger.debug("[players.update_player_status] " + inspect(player));
-      if (player) {
-        Logger.info("players.update_player_status: " + status.id);
-        player.status = status;
-        player.timeout = self.idle_watch(status.id);
-        db.saveDoc(player);
-      }
-      else {
-        Logger.warn("[players.update_player_status] unknown player: " + status.id + "!");
-      }
-    });
-  },
-
-  timeout: 30*60*1000,
-  timeouts: { },
-
-  idle_watch: function(id, timeout) {
-    if (this.timeouts[id])
-      clearTimeout(this.timeouts[id]);
-
-    if (!timeout) timeout = this.timeout;
-
-    var self = this;
-    this.timeouts[id] = setTimeout(function() {
-      Logger.info("timeout " + id +"!");
-      self.drop_player(id);
-    }, timeout);
-
-    return (new Date((new Date()).getTime() + timeout)).getTime();
-  },
-
-  drop_player: function(id) {
-    Logger.info("players.drop_player " + id);
-    this.faye.publish("/players/drop", id);
-
-    this.get(id, function(player) {
-      Logger.debug("[players.drop_player] " + inspect(player));
-      if (player) db.removeDoc(id, player._rev);
-    });
-  },
-
-  init_subscriptions: function() {
-    var faye = require("faye");
-    this.faye = new faye.Client('http://localhost:4011/faye');
-
-    var self = this;
-    this.faye.subscribe("/players/move", function(message) {
-      Logger.debug("[/players/move] " + inspect(message));
-      self.update_player_status(message);
-    });
-
-    this.faye.subscribe("/players/create", function(player) {
-      Logger.debug("[/players/create] " + inspect(player));
-      self.add_player(player);
-    });
-
-    this.faye.subscribe("/players/query", function(q) {
-      self.all(function(players) {
-        self.faye.publish("/players/all", players.map(function(player) {return player.status;}) );
-      });
-    });
-  },
-
-  init_players: function() {
-    var self = this;
-
-    var now = (new Date()).getTime();
-    self.all(function(players) {
-      players.forEach(function(player){
-        var timeout = player.timeout - now;
-        if (timeout > 0) {
-          Logger.info("[init_timeouts] " + player._id + " " + timeout);
-          self.idle_watch(player._id, timeout);
-        }
-        else {
-          Logger.info("[init_timeouts] dropping: " + player._id);
-          self.drop_player(player._id);
-        }
-      });
-    });
-  },
-
-  init: function() {
-    var self = this;
-
-    // Ensure that the faye server has fully established by waiting
-    // half a second before subscribing to channels
-    setTimeout(function(){
-      self.init_subscriptions();
-      self.init_players();
-    }, 500);
-
-    return self;
-  }
-}).init();
+// Local (fab) apps
 
 function player_status(write) {
   return write(function(write) {
@@ -298,4 +164,23 @@ function player_status(write) {
       });
     });
   });
+}
+
+
+// Additional functions
+
+function attach_faye(server) {
+  var faye_server = new faye.NodeAdapter({
+    mount:    '/faye',
+    timeout:  45
+  });
+
+  faye_server.attach(server);
+
+  // attach the extension ensuring player messages come from the same
+  // client that originally added player to the room
+  var auth = require("./lib/faye_server_auth").init(function () {
+    players.get.apply(players, arguments);
+  });
+  faye_server.addExtension(auth);
 }
